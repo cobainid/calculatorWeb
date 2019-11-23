352cea0e93efa435c90924a39917ab13455e2dcf
defrindr <defrindr@gmail.com>
2019-11-23 18:09:53 +0700

    Updated v1

--- file.js
+++ file.js
@@ -1,40 +1,71 @@
 
 
-const display = document.getElementById("display");
-const ret = document.getElementById("ret");
+const display = document.getElementById("display"),
+	ret = document.getElementById("ret"),
+	modal = document.getElementById('modal'),
+	info = document.getElementById('info'),
+	modalCancel = document.getElementById('modal-cancel');
 
 
+/**
+ * Btn Modal
+ */
+
+modalCancel.addEventListener('click',function(){
+	modal.style.display = 'none';
+});
+
+info.addEventListener('click',function(){
+	modal.style.display = 'block';
+});
+/**
+ * End Modal
+ */
+
+
+// clear window
 function hapus(){
 	display.innerHTML = "0";
-	ret.innerHTML = "0";
-	// calculate();
+	ret.innerHTML = "";
 }
 
+// calculating function
 function calculate(){
-	 ret.innerHTML = eval(display.innerHTML);
+	ret.innerHTML = eval(display.innerHTML);
 	 // console.log(display.innerHTML)
 }
+
+// add to display
 function adding(num){
 	if(display.innerHTML == 0){
-		display.innerHTML = num;
-		calculate()
+		if(num == "/" || num == "+" || num == "-" || num == "*"){
+			alert('Error');
+		}else{
+			display.innerHTML = num;
+		}
 	}else {
-		display.innerHTML += num;
-		calculate();
+		inp = display.innerHTML.slice(display.innerHTML.length - 1, display.innerHTML.length);
+		
+		if ((inp == "+" || inp == "/" || inp == "-" || inp == "*") && (num == "/" || num == "+" || num == "-" || num == "*")) {
+			alert('Operator ganda !!');
+		}else{
+			
+			display.innerHTML += num;
+		}
 	}
 }
+
+// show result from calculate()
 function equals(){
+	calculate();
 	display.innerHTML = ret.innerHTML;
-	ret.innerHTML = "0";
+	ret.innerHTML = "";
 }
+
 function del(){
 	var uy = display.innerHTML;
 	display.innerHTML = uy.slice(0,uy.length-1);
 	if(display.innerHTML == ""){
 		display.innerHTML = "0";
 	}
-	calculate();
 }
-document.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
-});
